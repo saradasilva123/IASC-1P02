8782dc0574ebe86f6cb02a91bd500cb593a0a1a2
saradasilva123 <71158687+saradasilva123@users.noreply.github.com>
2020-12-06 16:07:58 -0500

    upload

---
+++ Assignment 4/Page2.js
@@ -0,0 +1,32 @@
+var todayDate = new Date();
+
+var month = todayDate.getMonth()+1
+
+var day = todayDate.getDate();
+
+var year = todayDate.getFullYear();
+
+console.log(day+"/"+month+"/"+year);
+
+console.log(todayDate);
+console.log(day);
+console.log(month);
+console.log(year);
+
+document.getElementById('output').innerHTML=day+"/"+month+"/"+year;
+
+var todayDate = new Date();
+
+var hours = todayDate.getHours();
+var minutes = todayDate.getMinutes();
+var seconds = todayDate.getSeconds();
+
+console.log (hours+"/"+minutes+"/"+seconds);
+console.log(todayDate);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
+
+document.getElementById('time').innerHTML=hours+":"+minutes+":"+seconds;
+
+window.alert("Check out my favourite travel spot! :)");
